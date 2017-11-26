9c9c5ab2a647d366e89aead12d88a9433444da00
DoctrinaManagement <doctrinamanagement@gmail.com>
2017-11-26 16:12:48 +0530

    Signup And signin test

--- src/main/java/database/DatabaseConnection.java
+++ src/main/java/database/DatabaseConnection.java
@@ -1,5 +1,24 @@
 package database;
 
+import java.sql.*;
+/** 
+ * @author Pandeeswaran
+ */
 public class DatabaseConnection {
-
+	private final String url = "jdbc:postgresql://35.188.220.205:5432/doctrina";
+	//private final String url = "jdbc:postgresql://localhost:5432/doctrina";
+    private final String user = "postgres";
+    private final String password = "postgres";
+    
+    public Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            //log.info(url);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage()+"   SQLException ");
+        }
+        return conn;
+    }
 }
--- src/main/java/sign/Signup.java
+++ src/main/java/sign/Signup.java
@@ -1,5 +1,72 @@
 package sign;
+import java.io.*;
+import javax.servlet.http.*; // import javax.servlet.ServletException;
 
-public class Signup {
+import database.DatabaseConnection;
+import export.ReUsable;
 
+import java.util.*; // import javax.servlet.http.HttpServletRequest;
+import javax.servlet.*;
+import java.sql.*;
+/** 
+ * @author Basheer
+ */
+public class Signup extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		PrintWriter writer = response.getWriter();
+		DatabaseConnection data = new DatabaseConnection();
+		Connection connection = data.connect();
+		Statement stmt = null;
+		HttpSession session = request.getSession();
+	    ReUsable get = new ReUsable();
+		String userId = "";
+		try {
+			stmt = connection.createStatement();
+			userId = "'" + request.getParameter("user_id") + "'";
+			String name = "'" + request.getParameter("name") + "'";
+			String emailId = "'" + request.getParameter("email_id") + "'";
+			String image = "'" + request.getParameter("image") + "'";
+			String role = "'" + request.getParameter("role") + "'";
+			String notification = "'"
+					+ "Welcome to Doctrina. Learn Anything easy and better ..."
+					+ "'";
+
+
+			stmt.executeUpdate("insert into userdetails values(" + userId + ","
+					+ name + "," + emailId + "," + image + "," + role + ");");
+			ResultSet rs = stmt.executeQuery("select * from userdetails where user_id = "
+					+ userId);
+			HashMap<String, String> details = get.resultSetToHashMap(rs);
+			String status = "'"+"true"+"'";
+			String sender = "'"+"1"+"'";
+			String Query = "insert into notification values(" + userId+ "," + notification + "," + status +","+sender +");";
+			stmt.executeUpdate(Query);
+
+			// Session setUP ...
+			for (String values : details.keySet()) {
+				session.setAttribute(values, details.get(values));
+			}
+			
+			// cookie create ...
+//			String cookie = get.CookieCreate();
+//			Cookie cookies=new Cookie("Name", cookie);//creating cookie object  
+//            response.addCookie(cookies);//adding cookie in the response  
+//            
+//            cookie = "'"+cookie+"'";
+//            String user_id = "'"+details.get("user_id")+"'";
+//            
+//            try {
+//                stmt.executeUpdate("insert into cookie values("+cookie+","+user_id+");");
+//            }
+//            catch (SQLException e) {
+//                System.out.println(e);
+//            }
+			writer.write("ok");
+		} catch (SQLException e) {
+			System.out.println(e.getStackTrace());
+		}
+	}
 }
